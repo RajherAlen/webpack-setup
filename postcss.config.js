9bf3e9362b2f194139fea8b3f49e384d759473fb
Alen Rajher <alen.rajher@gmail.com>
2022-04-10 09:52:13 +0200

    change postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,37 +1,48 @@
-const paths = require("./config/paths");
+const postcssImport = require('postcss-import')
+const normalize = require('postcss-normalize')
+const postCssMixins = require('postcss-mixins')
+const postcssSimpleVars = require('postcss-simple-vars')
+const postcssNested = require('postcss-nested')
+const postcssCustomMedia = require('postcss-custom-media')
+const postcssResponsiveType = require('postcss-responsive-type')
+const postcssColorFunction = require('postcss-color-function')
+const postcssInlineSvg = require('postcss-inline-svg')
+const postcssCalc = require('postcss-calc')
+const autoprefixer = require('autoprefixer')
+const paths = require('./config/paths')
 
 module.exports = {
-	plugins: [
-		require("postcss-import")(
-			require("postcss-normalize")().postcssImport({
-				path: [paths.appSrc]
-			})
-		),
-		require("postcss-mixins"),
-		require("postcss-simple-vars"),
-		require("postcss-nested"),
-		require("postcss-custom-media"),
-		require("postcss-responsive-type"),
-		require("postcss-color-function"),
-		require("postcss-inline-svg"),
-		require("postcss-calc"),
-		require("autoprefixer")({
-			browserlist: [
-				">1%",
-				"last 4 versions",
-				"Firefox ESR",
-				"not ie < 9" // React doesn't support IE8 anyway
-			],
-			flexbox: "no-2009"
-		})
-		// require("cssnano")({
-		//     preset: [
-		//         "default",
-		//         {
-		//             discardComments: { removeAll: true },
-		//             svgo: true,
-		//         },
-		//     ],
-		// }),
-	]
-};
+    plugins: [
+        postcssImport(
+            normalize().postcssImport({
+                path: [paths.appSrc],
+            })
+        ),
+        postCssMixins,
+        postcssSimpleVars,
+        postcssNested,
+        postcssCustomMedia,
+        postcssResponsiveType,
+        postcssColorFunction,
+        postcssInlineSvg,
+        postcssCalc,
+        autoprefixer({
+            browserlist: [
+                '>1%',
+                'last 4 versions',
+                'Firefox ESR',
+                'not ie < 9', // React doesn't support IE8 anyway
+            ],
+            flexbox: 'no-2009',
+        }),
+        // require("cssnano")({
+        //     preset: [
+        //         "default",
+        //         {
+        //             discardComments: { removeAll: true },
+        //             svgo: true,
+        //         },
+        //     ],
+        // }),
+    ],
+}
